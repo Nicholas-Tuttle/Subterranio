4258a310d03570af24093edd2ae67af41dfe65f0
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2024-12-30 08:05:17 -0600

    Adding mineshaft belt variations

--- source/prototypes/entity/mineshaft-belt.lua
+++ source/prototypes/entity/mineshaft-belt.lua
@@ -1,25 +1,39 @@
-local mineshaft_belt_object = table.deepcopy(data.raw["linked-belt"]["linked-belt"])
-mineshaft_belt_object.name = "mineshaft-belt"
+-- name -> String
+-- speed -> Float
+-- underground_belt_ingredient -> String
+-- returns -> {object, item, recipe}
+local function create_mineshaft_belt_type(name, speed, underground_belt_ingredient)
+    local object = table.deepcopy(data.raw["linked-belt"]["linked-belt"])
+    object.name = name
+    object.speed = speed
 
-local mineshaft_belt_item = {
-    type = "item",
-    name = "mineshaft-belt",
-    stack_size = 1,
-    icon = "__base__/graphics/icons/linked-belt.png",
-    place_result = "mineshaft-belt"
-}
+    local item = {
+        type = "item",
+        name = name,
+        stack_size = 50,
+        hidden = false,
+        icon = "__base__/graphics/icons/linked-belt.png",
+        place_result = name
+    }
 
-local mineshaft_belt_recipe = {
-    type = "recipe",
-    name = "mineshaft-belt",
-    enabled = true,
-    energy_requirements = 1,
-    ingredients = {
-        {type = "item", name = "low-density-structure", amount = 5},
-        {type = "item", name = "steel-plate", amount = 10},
-        {type = "item", name = "concrete", amount = 10}
-    },
-    results = {{type = "item", name = "mineshaft-belt", amount = 1}}
-}
+    local recipe = {
+        type = "recipe",
+        name = name,
+        enabled = true,
+        energy_requirements = 1,
+        ingredients = {
+            {type = "item", name = underground_belt_ingredient, amount = 10},
+            {type = "item", name = "low-density-structure", amount = 5},
+            {type = "item", name = "steel-plate", amount = 10},
+            {type = "item", name = "concrete", amount = 10}
+        },
+        results = {{type = "item", name = name, amount = 1}}
+    }
 
-data:extend{mineshaft_belt_object, mineshaft_belt_item, mineshaft_belt_recipe}
+    return {object, item, recipe}
+end
+
+data:extend(create_mineshaft_belt_type("mineshaft-belt", 0.03125, "underground-belt"))
+data:extend(create_mineshaft_belt_type("fast-mineshaft-belt", 0.0625, "fast-underground-belt"))
+data:extend(create_mineshaft_belt_type("express-mineshaft-belt", 0.09375, "express-underground-belt"))
+data:extend(create_mineshaft_belt_type("turbo-mineshaft-belt", 0.125, "turbo-underground-belt"))
--- source/scripts/entity/mineshaft-belt.lua
+++ source/scripts/entity/mineshaft-belt.lua
@@ -1,38 +1,42 @@
-local function built_entity(event)
-    if event.entity.name ~= "mineshaft-belt" then return end
-    
+local function built_mineshaft(event)
     local mineshaft_belt_entrance = event.entity
     local position = mineshaft_belt_entrance.position
     local opposite_surface_name = (mineshaft_belt_entrance.surface.name == "nauvis" and "subterrain" or "nauvis")
     
     -- TODO: Make sure the other surface has open area where trying to build
-    -- TODO: Can this show a UI camera view of the other surface?
+    -- TODO: Can this show a camera view UI of the other surface?
 
     local target_surface = game.planets[opposite_surface_name].create_surface()
     local target_position = {x = position.x, y = position.y}
     target_surface.request_to_generate_chunks(target_position, 1)
     target_surface.force_generate_chunk_requests()
 
-    local mineshaft_belt_exit = target_surface.create_entity{name = "mineshaft-belt", position = position, force = game.forces.player}
+    local mineshaft_belt_exit = target_surface.create_entity{name = event.entity.name, position = position, force = game.forces.player}
 
     mineshaft_belt_entrance.linked_belt_type = "input"
     mineshaft_belt_exit.linked_belt_type = "output"
     mineshaft_belt_entrance.connect_linked_belts(mineshaft_belt_exit)
 end
 
-local function destroyed_entity(event)
-    if event.entity.name ~= "mineshaft-belt" then return end
+local function destroyed_mineshaft(event)
 
     local position = event.entity.position
     local opposite_surface_name = (event.entity.surface.name == "nauvis" and "subterrain" or "nauvis")
-    local mineshaft_to_destroy = game.surfaces[opposite_surface_name].find_entity("mineshaft-belt", position)
+    local mineshaft_to_destroy = game.surfaces[opposite_surface_name].find_entity(event.entity.name, position)
     if mineshaft_to_destroy ~= nil then
         mineshaft_to_destroy.destroy()
     end
 end
 
-script.on_event(defines.events.on_built_entity, built_entity)
-script.on_event(defines.events.on_robot_built_entity, built_entity)
-script.on_event(defines.events.on_pre_player_mined_item , destroyed_entity)
-script.on_event(defines.events.on_entity_died , destroyed_entity)
-script.on_event(defines.events.on_robot_pre_mined  , destroyed_entity)
+local mineshaft_belt_filter = {
+    {filter = "name", name = "mineshaft-belt"},
+    {filter = "name", name = "fast-mineshaft-belt"},
+    {filter = "name", name = "express-mineshaft-belt"},
+    {filter = "name", name = "turbo-mineshaft-belt"}
+}
+
+script.on_event(defines.events.on_built_entity, built_mineshaft, mineshaft_belt_filter)
+script.on_event(defines.events.on_robot_built_entity, built_mineshaft, mineshaft_belt_filter)
+script.on_event(defines.events.on_pre_player_mined_item , destroyed_mineshaft, mineshaft_belt_filter)
+script.on_event(defines.events.on_entity_died , destroyed_mineshaft, mineshaft_belt_filter)
+script.on_event(defines.events.on_robot_pre_mined  , destroyed_mineshaft, mineshaft_belt_filter)

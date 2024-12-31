a00ac76e58fe0afd22f56bb5bf3a01169144804b
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2024-12-30 19:54:11 -0600

    Simplifying mineshaft belts

--- source/prototypes/entity/mineshaft-belt.lua
+++ source/prototypes/entity/mineshaft-belt.lua
@@ -2,38 +2,39 @@
 -- speed -> Float
 -- underground_belt_ingredient -> String
 -- returns -> {object, item, recipe}
-local function create_mineshaft_belt_type(name, speed, underground_belt_ingredient)
+local function create_mineshaft_belt_type(prefix, speed)
     local object = table.deepcopy(data.raw["linked-belt"]["linked-belt"])
-    object.name = name
+    object.name = prefix .. "mineshaft-belt"
     object.speed = speed
+    object.minable = {mining_time = 0.1, result = prefix .. "mineshaft-belt"}
 
     local item = {
         type = "item",
-        name = name,
+        name = prefix .. "mineshaft-belt",
         stack_size = 50,
         hidden = false,
         icon = "__base__/graphics/icons/linked-belt.png",
-        place_result = name
+        place_result = prefix .. "mineshaft-belt"
     }
 
     local recipe = {
         type = "recipe",
-        name = name,
+        name = prefix .. "mineshaft-belt",
         enabled = true,
         energy_requirements = 1,
         ingredients = {
-            {type = "item", name = underground_belt_ingredient, amount = 10},
+            {type = "item", name = prefix .. "underground-belt", amount = 10},
             {type = "item", name = "low-density-structure", amount = 5},
             {type = "item", name = "steel-plate", amount = 10},
             {type = "item", name = "concrete", amount = 10}
         },
-        results = {{type = "item", name = name, amount = 1}}
+        results = {{type = "item", name = prefix .. "mineshaft-belt", amount = 1}}
     }
 
     return {object, item, recipe}
 end
 
-data:extend(create_mineshaft_belt_type("mineshaft-belt", 0.03125, "underground-belt"))
-data:extend(create_mineshaft_belt_type("fast-mineshaft-belt", 0.0625, "fast-underground-belt"))
-data:extend(create_mineshaft_belt_type("express-mineshaft-belt", 0.09375, "express-underground-belt"))
-data:extend(create_mineshaft_belt_type("turbo-mineshaft-belt", 0.125, "turbo-underground-belt"))
+data:extend(create_mineshaft_belt_type("", 0.03125))
+data:extend(create_mineshaft_belt_type("fast-", 0.0625))
+data:extend(create_mineshaft_belt_type("express-", 0.09375))
+data:extend(create_mineshaft_belt_type("turbo-", 0.125))

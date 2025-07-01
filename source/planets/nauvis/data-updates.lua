2de353f760f8d585b75d8d205342916ea6f6c0fd
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2025-06-30 20:20:03 -0500

    Updating tech requirements with subterranean science packs

--- source/planets/nauvis/data-updates.lua
+++ source/planets/nauvis/data-updates.lua
@@ -5,3 +5,17 @@ table.insert(data.raw["furnace"]["stone-furnace"].crafting_categories, "smelting
 table.insert(data.raw["furnace"]["steel-furnace"].crafting_categories, "smelting-or-metallurgy")
 table.insert(data.raw["furnace"]["electric-furnace"].crafting_categories, "smelting-or-metallurgy")
 table.insert(data.raw["assembling-machine"]["foundry"].crafting_categories, "smelting-or-metallurgy")
+
+table.insert(data.raw["technology"]["planet-discovery-aquilo"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["planet-discovery-aquilo"].prerequisites, "subterranean-science-pack")
+
+table.insert(data.raw["technology"]["quantum-processor"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["captive-biter-spawner"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["foundation"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["fusion-reactor"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["fusion-reactor-equipment"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["legendary-quality"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["railgun"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["railgun-damage-1"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["promethium-science-pack"].unit.ingredients, {"subterranean-science-pack", 1})
+table.insert(data.raw["technology"]["research-productivity"].unit.ingredients, {"subterranean-science-pack", 1})

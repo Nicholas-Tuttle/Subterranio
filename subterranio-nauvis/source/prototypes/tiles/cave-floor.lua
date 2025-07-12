483ae334493e61bc07f5b5a81632a8f570aaf7a4
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2025-07-12 17:07:23 -0500

    Deleting unneeded cave floor

--- subterranio-nauvis/source/prototypes/tiles/cave-floor.lua
+++ subterranio-nauvis/source/prototypes/tiles/cave-floor.lua
@@ -1,10 +1,3 @@
--- local cave_floor = table.deepcopy(data.raw["tile"]["dirt-1"])
--- cave_floor.name = "cave-floor"
--- cave_floor.order = "z[subterrain]-a[cave-floor][dirt-1]"
--- cave_floor.autoplace.probability_expression = "1 - subterranean_impassable_cliffs_ridge_noise_expression"
-
--- data:extend{cave_floor}
-
 local tiles = {
     "volcanic-soil-dark",
     "volcanic-soil-light",

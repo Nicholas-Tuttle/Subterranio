cbe44f10774fcc049f0c97c32b8001ff934d8394
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2025-07-13 22:02:55 -0500

    Update cave scaffolding tech and recipe

--- subterranio-nauvis/source/prototypes/tiles/cave-scaffolding.lua
+++ subterranio-nauvis/source/prototypes/tiles/cave-scaffolding.lua
@@ -19,13 +19,55 @@ item.place_as_tile =
   condition = { layers = {} },
   tile_condition = { "cave-wall" }
 }
+item.enabled = false
 
 local tech = {
-
+  type = "technology",
+  name = "cave-scaffolding",
+  icon = data.raw["technology"]["landfill"].icon,
+  icon_size = data.raw["technology"]["landfill"].icon_size,
+  effects = {
+    {
+      type = "unlock-recipe",
+      recipe = "cave-scaffolding"
+    }
+  },
+  prerequisites = {
+    "chemical-science-pack",
+    "subterranean-science-pack",
+    "landfill",
+    "steel-processing",
+    "low-density-structure",
+    "explosives",
+    "nuclear-power"
+  },
+  unit =
+  {
+    count = 100,
+    ingredients =
+    {
+      { "automation-science-pack",   1 },
+      { "logistic-science-pack",     1 },
+      { "chemical-science-pack",     1 },
+      { "subterranean-science-pack", 1 }
+    },
+    time = 60
+  }
 }
 
 local recipe = {
-
+  type = "recipe",
+  name = "cave-scaffolding",
+  enabled = false,
+  energy_requirements = 20,
+  ingredients = {
+    { type = "item", name = "landfill",           amount = 1 },
+    { type = "item", name = "steel",              amount = 2 },
+    { type = "item", name = "low-density-structure", amount = 1 },
+    { type = "item", name = "explosives",           amount = 1 },
+    { type = "item", name = "heat-pipe",           amount = 1 }
+  },
+  results = { { type = "item", name = "cave-scaffolding", amount = 1 } }
 }
 
-data:extend { tile, item }
+data:extend { tile, item, tech, recipe }

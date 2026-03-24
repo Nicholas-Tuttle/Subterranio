f52456fd17f4a0ecbcdc4268261a9ef0b43b4ff5
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2026-03-24 17:26:03 -0500

    Combining constants file to fix bug with not being able to craft science pack

--- subterranio-vulcanus/source/data.lua
+++ subterranio-vulcanus/source/data.lua
@@ -18,7 +18,6 @@ require("prototypes.items.propulsion-science-pack")
 require("prototypes.items.subgroups")
 require("prototypes.items.titanium")
 
-require("prototypes.surfaces.constants")
 require("prototypes.surfaces.subterranean-noise-expressions")
 require("prototypes.surfaces.subterranean-surface")
 

7c249308e07c7587eff0d00dbf405ae21df1f021
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2026-06-28 16:09:01 -0500

    Removing starmap icon logs

--- subterranio-base/source/utilities/starmap-icon.lua
+++ subterranio-base/source/utilities/starmap-icon.lua
@@ -6,18 +6,18 @@ local function position_starmap_icon(parent_planet, subterranean_depth)
     local parent_orientation_radians = ((1.25 - parent_orientation) % 1) * 2 * math.pi
     local parent_x = math.cos(parent_orientation_radians) * parent_distance
     local parent_y = math.sin(parent_orientation_radians) * parent_distance
-    log("Parent planet " .. parent_planet.name .. " orientation: " .. parent_orientation .. ", distance: " .. parent_distance)
-    log("Parent planet " .. parent_planet.name .. " Cartesian coordinates: x=" .. parent_x .. ", y=" .. parent_y)
+    -- log("Parent planet " .. parent_planet.name .. " orientation: " .. parent_orientation .. ", distance: " .. parent_distance)
+    -- log("Parent planet " .. parent_planet.name .. " Cartesian coordinates: x=" .. parent_x .. ", y=" .. parent_y)
 
     -- Calculate the starmap icon's position by offsetting from the parent planet's position
     local subterrain_y = parent_y - (subterranean_depth * 2)
-    log("Starmap icon " .. parent_planet.name .. " Cartesian coordinates: x=" .. parent_x .. ", y=" .. subterrain_y)
+    -- log("Starmap icon " .. parent_planet.name .. " Cartesian coordinates: x=" .. parent_x .. ", y=" .. subterrain_y)
     -- Convert back to Factorio RealOrientation coordinates to get the starmap icon's orientation and distance
     local starmap_icon_distance = math.sqrt(parent_x^2 + subterrain_y^2)
     local starmap_icon_orientation_radians = math.atan(subterrain_y / parent_x)
     local starmap_icon_orientation = (1.25 - starmap_icon_orientation_radians / (2 * math.pi)) % 1
-    log("Starmap icon " .. parent_planet.name .. " distance: " .. starmap_icon_distance)
-    log("Starmap icon " .. parent_planet.name .. " orientation: " .. starmap_icon_orientation)
+    -- log("Starmap icon " .. parent_planet.name .. " distance: " .. starmap_icon_distance)
+    -- log("Starmap icon " .. parent_planet.name .. " orientation: " .. starmap_icon_orientation)
     return {
         starmap_icon_orientation = starmap_icon_orientation,
         starmap_icon_distance = starmap_icon_distance
